6de126776410c9a3d0971cc215ccb94bcfe957e0
lucien <zimwolfgang24@gmail.com>
2023-01-30 17:08:13 -0400

    feat: basic structure front end

---
+++ front-end/desafio-fullstack/src/style/global.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+    body{
+        background: #000000;
+    }
+    button{
+        cursor: pointer;
+        background: transparent;
+        border: none;
+    }
+    ul, ol {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+    }
+    li{
+        list-style: 0;
+    }
+    img{
+        max-width: 100%;
+    }
+`;
+
+export const ColorsTheme = {
+  colors: {
+    white: '#FFFFFF',
+    white20: 'rgba(217, 217, 217, 0.15)',
+    white50: 'rgba(255, 255, 255, .5)',
+    gray15: 'rgba(217, 217, 217, 0.15)',
+    red: '#F55859',
+    blue: '#0F81EC'
+  }
+};
